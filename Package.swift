9caeb931970d014564a76ba7009973f361413c48
renan <renan@aestesis.org>
2019-02-23 11:14:05 +0100

    ok?

--- Package.swift
+++ Package.swift
@@ -1,13 +1,12 @@
 // swift-tools-version:4.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CFontConfig",
     products: [
-        .library(name: "CFontConfig",targets: ["CFontConfig"]),
-        ],
+        .library(name: "CFontConfig",targets: ["CFontConfig"])
+    ],
     dependencies: [
     ],
     targets: [
